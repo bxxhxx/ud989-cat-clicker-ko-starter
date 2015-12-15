4c3a94f9f93a6e28e7021e9e7d775ed930d6700b
bxxhxx <Tom@New-Box-17-Edit.local>
2015-12-14 17:42:54 -0800

    with catLevel
    took the blank repository and made one cat and in the second try made
    cat level ko computed observables

---
+++ js/app.js
@@ -0,0 +1,21 @@
+var ViewModel = function() {
+    this.clickCount = ko.observable(0);
+    this.name = ko.observable('Tabby');
+    this.imgSrc = ko.observable('img/22252709_010df3379e_z.jpg');
+
+    this.incrementCounter = function() {
+        this.clickCount(this.clickCount() + 1);
+    };
+
+    this.catLevel = ko.computed(function() {
+        if (this.clickCount() < 10) {
+            return "New Born";
+        } else if (this.clickCount() < 20) {
+            return "Infant";
+        } else {
+            return "Teen";
+        }
+    }, this);
+};
+
+ko.applyBindings(new ViewModel());
